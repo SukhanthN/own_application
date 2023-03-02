07be7a23c78bc047cd862eab2e71e888d93e6084
sukhanth <sukhanth.nandam@gmail.com>
2023-03-03 01:24:39 +0530

    add hello world file

--- web/src/main/java/com/suhkanth/login/LoginApplication.java
+++ web/src/main/java/com/suhkanth/login/LoginApplication.java
@@ -2,9 +2,16 @@ package com.suhkanth.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LoginApplication {
+
+	@GetMapping
+	public String message(){
+		return "Hello world this sukhanth";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
